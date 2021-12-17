4748c8383464e8e98e772afb658c7e0ae73cb83f
jai <93277391+jayaprakash287@users.noreply.github.com>
2021-12-16 20:15:24 -0800

    Add files via upload

---
+++ code/setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(
+    install_requires=[
+        "its-a-demo==0.0.1"
+    ]
+)
---
+++ code/src/file.py
@@ -0,0 +1,10 @@
+def print_hello():
+    print('hello world')
+
+class myclass:
+    def __init__(self,word):
+        self.word=word
+
+    def print(self):
+        print(self.word)
+
